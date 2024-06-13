c6e8a571565bdf715f1b18999035bf2e4d81bd65
Pac4 <amanoskull42@gmail.com>
2024-06-13 12:08:00 +0100

    Slight adjustments to non-42 students that would wish to use my config

--- .config/nvim/lua/core/plugin_config/42header.lua
+++ .config/nvim/lua/core/plugin_config/42header.lua
@@ -1,2 +1,4 @@
-vim.cmd("let g:user42 = 'paugonca'")
-vim.cmd("let g:mail42 = 'paugonca@student.42lisboa.com'")
+-- Only required if using 42 school's header plugin
+
+-- vim.cmd("let g:user42 = 'marvin'")
+-- vim.cmd("let g:mail42 = 'marvin@42.fr'")
