a51685dda0914727f1df52d0393a38e528bedbae
Pac4games <paulo.nicolau7@gmail.com>
2024-09-25 12:36:58 +0100

    Updated clear_containers.sh

--- .myscripts/clear_containers.sh
+++ .myscripts/clear_containers.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
-
 echo "Cleaning up Docker containers, volumes and networks..."
-docker stop $(docker ps -qa) > /dev/null
-docker rm $(docker ps -qa) > /dev/null
-docker rmi -f $(docker images -qa) > /dev/null
-docker volume rm $(docker volume ls -q) > /dev/null
+docker stop $(docker ps -qa) > /dev/null 2>&1
+docker rm $(docker ps -qa) > /dev/null 2>&1
+docker rmi -f $(docker images -qa) > /dev/null 2>&1
+docker volume rm $(docker volume ls -q) > /dev/null 2>&1
 docker network ls -q | while read network_id; do \
    network_name=$(docker network inspect -f '{{.Name}}' $network_id); \
    if [ "$network_name" != "bridge" ] && [ "$network_name" != "host" ] && [ "$network_name" != "none" ]; then \
-   	docker network rm $network_id > /dev/null; \
+   	docker network rm $network_id > /dev/null 2>&1; \
    fi; \
 done
 echo "Cleanup complete."
