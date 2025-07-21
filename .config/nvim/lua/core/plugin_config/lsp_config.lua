1539d0dc2d0bee4954e3003c0f81e6918c80b82a
nicolau <paulo.nicolau@nokia.com>
2025-07-21 11:55:19 +0100

    Removed vim-fugitive since it was interfering with Neotree's ability to hide dotfiles and .gitignore files

--- .config/nvim/lua/core/plugin_config/lsp_config.lua
+++ .config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -4,7 +4,6 @@ require("mason-lspconfig").setup({
 	ensure_installed = { 
 		"lua_ls",
 		"clangd",
-		"gopls",
 	},
 })
 
--- .config/nvim/lua/core/plugin_config/neotree.lua
+++ .config/nvim/lua/core/plugin_config/neotree.lua
@@ -3,6 +3,10 @@ require("neo-tree").setup {
 		window = {
 			width = 17
 		},
-		use_libuv_file_watcher=false,
+		use_libuv_file_watcher = true,
+		filtered_items = {
+			hide_dotfiles = true,
+			hide_gitignored = true,
+		},
 	},
 }
