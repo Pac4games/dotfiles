5c67fe1cea4c104c3c1ed94660b8713134735f81
nicolau <paulo.nicolau@nokia.com>
2025-08-14 15:12:46 +0100

    Added proper LSP configuration for YAML files and Kubernetes Helm charts

--- .config/nvim/lua/core/plugin_config/lsp_config.lua
+++ .config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -51,3 +51,49 @@ require("lspconfig").gopls.setup {
     },
   },
 }
+
+require("lspconfig").yamlls.setup {
+  settings = {
+	yaml = {
+	  format = { enable = true },
+	  validate = true,
+	  hover = true,
+	  completion = true,
+	  schemas = {
+        -- Example: Kubernetes schemas
+        ["https://json.schemastore.org/kustomization.json"] = "kustomization.yaml",
+        ["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*",
+      },
+      schemaStore = {
+        enable = true, -- Auto-download schemas
+        url = "https://www.schemastore.org/api/json/catalog.json",
+	  },
+      customTags = {
+        "!And scalar", "!And mapping", "!And sequence",
+        "!If scalar", "!If mapping", "!If sequence",
+        "!Not scalar", "!Not mapping", "!Not sequence",
+        "!Equals scalar", "!Equals mapping", "!Equals sequence",
+        "!Or scalar", "!Or mapping", "!Or sequence",
+        "!FindInMap scalar", "!FindInMap mapping", "!FindInMap sequence",
+        "!Base64 scalar", "!Cidr scalar", "!Ref scalar",
+        "!Ref mapping", "!Ref sequence",
+        "!Sub scalar", "!Sub mapping", "!Sub sequence",
+        "!GetAtt scalar", "!GetAtt mapping", "!GetAtt sequence",
+        "!GetAZs scalar", "!ImportValue scalar",
+        "!Select scalar", "!Select mapping", "!Select sequence",
+        "!Split scalar", "!Split mapping", "!Split sequence",
+        "!Join scalar", "!Join mapping", "!Join sequence"
+      }
+	}
+  }
+}
+
+require("lspconfig").helm_ls.setup {
+  settings = {
+	["helm-ls"] = {
+	  yamlls = {
+		enabled = true,
+	  }
+    }
+  }
+}
