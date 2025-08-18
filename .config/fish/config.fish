b4485fafa10842b958ef35bbc6019cc1ad653829
nicolau <paulo.nicolau@nokia.com>
2025-08-18 14:31:24 +0100

    Added initial fish and starship configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Aliases
+alias nvim="$HOME/Applications/nvim.appimage"
+alias nv=nvim
+
+fish_add_path -a $HOME/local/.bin
+
+starship init fish | source
+
+fastfetch
