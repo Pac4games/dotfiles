11905f2de2007c43c4c5c2d0b9494220bbf2ddeb
nicolau <paulo.nicolau@nokia.com>
2025-08-21 11:05:33 +0100

    Added git aliases to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,13 +2,20 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Path for Python modules and other user installed binaries
+fish_add_path -a "$HOME/.local/bin"
+
 # Aliases
 alias nvim="$HOME/Applications/nvim.appimage"
 alias nv=nvim
 
-fish_add_path -a "$HOME/.local/bin"
+alias gp="git push"
+alias gcam="git add -A && git commit"
+alias gc="git checkout"
 
+# Loading starship prompt
 starship init fish | source
 
+# Aesthetic fluff
 printf "✱ Greetings.\n\n"
 fastfetch
