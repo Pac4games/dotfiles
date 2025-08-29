3d85b04a898f341b68c6971aac2059b63bae5dc9
nicolau <paulo.nicolau@nokia.com>
2025-08-29 12:15:54 +0100

    Fish config updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,9 +13,16 @@ alias gp="git push"
 alias gac="git add -A && git commit"
 alias gc="git checkout"
 
+# Environment variables
+set -gx JUMP_SERVER "rcp_user@10.110.107.118"
+set -gx MY_LAB "root@128.140.72.90"
+
 # Loading starship prompt
 starship init fish | source
 
 # Aesthetic fluff
+set_color red
 printf "✱ Greetings.\n\n"
+set_color normal
+
 fastfetch
